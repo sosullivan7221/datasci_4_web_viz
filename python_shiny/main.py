21e7852fa17650dae86701cd89c23f5f3642eb47
sosullivan7221 <sosullivan7221@gmail.com>
2023-09-26 04:31:46 -0400

    changing file path in shiny file due to working in local IDE

--- python_shiny/main.py
+++ python_shiny/main.py
@@ -5,9 +5,8 @@ from shiny import App, render, ui
 
 # Load data
 def load_data():
-    path = '/home/sean_osullivan/datasci_4_web_viz/dataset/massachusetts_data.csv'
+    path = r'C:\Users\Sean\LocalDocuments\Code\AHI\datasci_4_web_viz\dataset\massachusetts_data.csv'
     return pd.read_csv(path)
-
 df = load_data()
 df_sleep = df[(df['MeasureId'] == 'SLEEP') & (df['Data_Value_Type'] == 'Age-adjusted prevalence')]
 
